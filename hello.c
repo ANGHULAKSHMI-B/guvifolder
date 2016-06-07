36a3869cca251a7e99f3f998a460bee13d00ff77
ANGHULAKSHMI-B <anghubala9t6@gmail.com>
2016-06-07 14:15:35 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+int main(void)
+{
+	//char str[10]={'Hello'};
+	int i;
+	//len=strlen(str);
+//while(len>0)
+	for(i=1;i<=5;i++)
+	{
+		printf("\nHello");
+	}
+	// your code goes here
+	return 0;
+}
